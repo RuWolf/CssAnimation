6adf437999b0472840a515d3b20430cc774de0cb
RuWolf <rudolf.bexshtedt@mail.ru>
2020-03-25 18:40:05 +0300

    add logic

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const min = document.querySelector('#min');
 const hour = document.querySelector('#hour');
 const result = document.querySelector('#result');
 const valueChannel = document.querySelector('#value-channel');
+const valueTime = document.querySelector('#value-time');
 
-let cancel = 1; //количество каналов
+let channel = 1; //количество каналов
 let time = 0; //
+let factorTime = 1; // времянной коэф. Показывает количество сек. в зависимости от выбранной времяной шкалы
 
 short.addEventListener('click', (event) => {
   short.classList.add('active');
@@ -26,24 +28,50 @@ sec.addEventListener('click', (event) => {
   sec.classList.add('active');
   min.classList.remove('active');
   hour.classList.remove('active');
+  factorTime = 1;
 });
 
 min.addEventListener('click', (event) => {
   min.classList.add('active');
   sec.classList.remove('active');
   hour.classList.remove('active');
+  factorTime = 60;
 });
 
 hour.addEventListener('click', (event) => {
   hour.classList.add('active');
   sec.classList.remove('active');
   min.classList.remove('active');
+  factorTime = 3600;
 });
 
 valueChannel.addEventListener('click', event => {
-  console.log(event.target.value)
-  cancel = event.target.value
-  result.innerHTML = `NLab Speech ${time} + ${cancel} = ${time + cancel}`;
+  if (+event.target.value > 0) {
+    channel = +event.target.value;
+  } else {
+    channel = 1;
+  }
+  result.innerHTML = calcPrise();
 });
-result.innerHTML = `NLab Speech ${time} + ${cancel} = ${time + cancel}`;
+
+valueTime.addEventListener('click', event => {
+  time = event.target.value;
+  result.innerHTML = calcPrise();
+});
+
+calcPrise = () => {
+  if (channel === 1) {
+    let allTime = +time * factorTime;
+    let prise = Math.ceil(allTime / 15) * 10 / 100;
+    return `NLab Speech ${prise}₽`;
+  } else {
+    let doobleChannel = Math.ceil(channel / 2);
+    let channelTime = Math.ceil(time*factorTime);
+    if (channelTime < 15) {
+      channelTime = 15;
+    }
+    return `NLab Speech ${doobleChannel*channelTime/100}₽`
+  }
+};
+
 
